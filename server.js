a3853966757b8db8dc9b436c4e3b60030d1f0132
Limsukhyun <as17269@naver.com>
2023-10-17 11:16:14 +0900

    C.Ronaldo

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ async function run() {
     connection = await oracledb.getConnection({
       user: 'DWLG',
       password: 'DWLG01',
-      connectString: '103.129.187.74/PKWMS', 
+      connectString: '103.129.187.74:1521/PKWMS', 
     });
+
+    console.log('Connection established successfully!'); // 연결 성공 시 출력
 
   } catch (error) {
     console.error('Error:', error);
@@ -16,6 +18,7 @@ async function run() {
     if (connection) {
       try {
         await connection.close();
+        console.log('Connection closed successfully!'); // 연결 종료 시 출력
       } catch (error) {
         console.error('Error closing the database connection:', error);
       }
