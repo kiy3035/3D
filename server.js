c8204f265f2ea076cd6d77c60da7bdbd95330aef
Limsukhyun <as17269@naver.com>
2023-10-17 13:07:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ async function run() {
     });
 
     console.log('Connection established successfully!'); // 연결 성공 시 출력
+    // const sql = `INSERT INTO THREE_D_TEST (MAINFEST_NO, BL_NO, CNTR_NO, CNTR_TYP, CNTR_SIZE, INP_USR, INP_DT, UPD_USR, UPD_DT) 
+    //              VALUES ('TESTLSH101701', 'TESTLSH1017_001', 'TESTLSH1017','TE', '20', 'LSH', '20231017','LSH', '20231017')`;
+    // 쿼리 실행
+    // const result = await connection.execute(sql);
+    // await connection.commit();
+    // console.log('Data inserted successfully!');
 
   } catch (error) {
     console.error('Error:', error);
